053fa3e4714391237565b1269fc02fa6424daf63
pavellaura05 <pavellaura2003@gmail.com>
2026-03-02 19:56:17 -0400

    Primer commit

---
+++ migrations/20260302223935-create-mancuerna.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Mancuernas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      marca: {
+        type: Sequelize.STRING
+      },
+      precio: {
+        type: Sequelize.INTEGER
+      },
+      peso: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Mancuernas');
+  }
+};
